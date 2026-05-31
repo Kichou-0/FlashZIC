6c71bc1f8a50737a02c50ee26cd55730791adce3
Kichou <kichouuuuu@gmail.com>
2026-05-31 14:34:18 +0200

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -38,7 +38,7 @@ export default function Sidebar() {
        </NavLink>
       )}
        <div className="sidebar-footer">
-        <p>Dev by <span className="sidebar-footer-name">Kichou</span>© {new Date().getFullYear()} FlashZIC</p>
+        <p>Dev by <span className="sidebar-footer-name">Kichou</span> © {new Date().getFullYear()} FlashZIC</p>
       </div>
     </aside>
   )
